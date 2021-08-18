e7c11100e7aedf714e74d8c79fdb3a56f7553572
Nikolas Matias <nikolas.matias500@gmail.com>
2021-08-18 02:22:50 -0300

    Create Card component among other modifications.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Card imageMobile="./assets/logo.png"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 })
 </script>
 
 <style lang="scss">
+$veryDarkBlue: hsl(233, 47%, 7%);
+
+body {
+  overflow-x: hidden;
+  background-color: $veryDarkBlue;
+}
 #app {
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100vw;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <img :src="require(`${imageMobile}`)" alt="Image of the Card">
+    </div>
+    <div class="card-content">
+      <h1 class="card-title">{{title}}</h1>
+      <p class="card-description">{{description}}</p>
+      <div class="extra-informations">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+interface IExtraInformation {
+  number: string,
+  description: string
+}
+
+export default defineComponent({
+  name: 'Card',
+  props: {
+    imageMobile: String,
+    imageDesktop: String,
+    title: String,
+    description: String,
+    extraInformations: Array as PropType<IExtraInformation[]>
+  }
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
